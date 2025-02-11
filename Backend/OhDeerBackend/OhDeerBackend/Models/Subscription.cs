9b38a195dd6e8b2d2364e368c358987191461c47
Nicola Truscello <nicola_truscello@sluz.ch>
2025-02-11 15:12:37 +0100

    Made basic data models that closely resemble the ERM and Entity Model diagrams. They include inheritance and simple data validation.

---
+++ Backend/OhDeerBackend/OhDeerBackend/Models/Subscription.cs
@@ -0,0 +1,14 @@
+﻿using OhDeerBackend.Models.Base_Classes;
+using System.ComponentModel.DataAnnotations;
+
+namespace OhDeerBackend.Models
+{
+    public class Subscription : BaseEntity
+    {
+        [Required, MaxLength(50)]
+        public string SubscriptionName { get; set; }
+
+        public virtual ICollection<User> Users { get; set; } = new List<User>();
+    }
+
+}
---
+++ Backend/OhDeerBackend/OhDeerBackend/Models/User.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using OhDeerBackend.Models.Base_Classes;
+
+namespace OhDeerBackend.Models
+{
+    public class User : BaseEntity
+    {
+        [Required, MaxLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public string HashSalt { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        public int RepairRating { get; set; }
+        public int GeneralRating { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Address { get; set; }
+
+        [Required]
+        public int PhoneNumber { get; set; }
+
+        [Required, MaxLength(34)] // IBAN length
+        public string IBAN { get; set; }
+
+        // Foreign Keys
+        [Required]
+        public int UserTypeId { get; set; }
+        public virtual UserType UserType { get; set; }
+
+        [Required]
+        public int LanguageId { get; set; }
+        public virtual Language Language { get; set; }
+
+        [Required]
+        public int SubscriptionId { get; set; }
+        public virtual Subscription Subscription { get; set; }
+
+        // Navigation Properties
+        public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
+        public virtual ICollection<Purchase> Purchases { get; set; } = new List<Purchase>();
+    }
+
+}
