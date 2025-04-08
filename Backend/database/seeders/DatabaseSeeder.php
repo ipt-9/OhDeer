e2bfac74d8c2ce6c41a34717725773494b1f8c5e
Nicola Truscello <nicola_truscello@sluz.ch>
2025-04-08 16:58:44 +0200

    Made simple factories and started work on the seeder.

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Language;
+use App\Models\Subscription;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\UserType;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +19,26 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        UserType::factory()->create(
+            ['user_type_name' => 'Private']
+        );
+        UserType::factory()->create(
+            ['user_type_name' => 'Business']
+        );
+        Language::factory()->create(
+            ['language_name' => 'English']
+        );
+        Subscription::factory()->create(
+            ['subscription_name' => 'Regular']
+        );
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+            'user_type_id' => 1,
+            'language_id' => 1,
+            'subscription_id' => 1,
         ]);
     }
 }
